cade89b479dcdac7f060f96860a01d12cd2135e4
Dafne Villarreal <83681690+DafneVillarreal@users.noreply.github.com>
2022-03-09 18:29:06 -0600

    Create cola.h

---
+++ cola/cola.h
@@ -0,0 +1,72 @@
+#ifndef COLA_H
+#define COLA_H
+#include <iostream>
+#include <stdexcept>
+#include "ldl.h"
+
+using namespace std;
+
+template<typename T>
+class Cola // queue FIFO (First In First Out)
+{
+private:
+    LDL<T> lista;
+
+public:
+    Cola(){}
+    ~Cola()
+    {
+        lista.clear();
+    }
+    bool empty() const;
+    size_t size() const;
+    const T& front() const;
+    const T& back() const;
+    void push(const T& element);
+    void pop();
+};
+
+template<typename T>
+bool Cola<T>::empty() const
+{
+    return lista.empty();
+}
+
+template<typename T>
+size_t Cola<T>::size() const
+{
+    return lista.size();
+}
+
+template<typename T>
+const T& Cola<T>::front() const
+{
+    if (empty())
+    {
+        throw runtime_error("Trying front() from an empty queue.");
+    }
+    return lista.front();
+}
+
+template<typename T>
+const T& Cola<T>::back() const
+{
+    if (empty())
+    {
+        throw runtime_error("Trying back() from an empty queue.");
+    }
+    return lista.back();
+}
+
+template<typename T>
+void Cola<T>::push(const T& element)
+{
+    lista.push_back(element);
+}
+
+template<typename T>
+void Cola<T>::pop()
+{
+    lista.pop_front();
+}
+#endif // COLA_H
